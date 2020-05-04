65c110aa65e7f7dcca469e482dd9bc66202e4281
张亚欣 <x18932602130@163.com>
2020-05-05 03:00:40 +0800

    vur one

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
+    <Header />
     <!-- 显示当前配置的路由 -->
     <router-view></router-view>
+    <!-- 所有的组件都多了一个属性$route，以下是$route里面的功能path路径 显示和隐藏-->
+    <!-- <Footer v-show="$route.path !== '/register' && $route.path!=='/login'" /> -->
+    <Footer v-show="!$route.meta.isHideFooter" />
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 export default {
   name: "APP",
+  components: {
+    Header,
+    Footer,
+  },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,12 @@
 // 路由器对象模块
 import Vue from "vue";
 import VueRouter from "vue-router";
-// 引入文件
-import Home from "@/pages/Home";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
-import Search from "@/pages/Search";
+import routes from "./routes";
+
 // 声明使用vue插件
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  // 配合所有路由
-  routes: [
-    {
-      path: "/",
-      component: Home,
-    },
-    {
-      path: "/search",
-      component: Search,
-    },
-    {
-      path: "/register",
-      component: Register,
-    },
-    {
-      path: "/login",
-      component: Login,
-    },
-  ],
+  mode: "history", //没有#号的模式
+  routes, //配置所有路由
 });
